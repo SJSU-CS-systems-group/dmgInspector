62d2b186e2a2df1e2f5a6a19792d51efc9a4aa7b
Sivam Agarwalla <sivam.agarwalla@gmail.com>
2021-09-06 17:12:00 -0700

    GPT size and sectors working too.

--- src/GPTPartitionEntry.java
+++ src/GPTPartitionEntry.java
@@ -9,7 +9,7 @@ public class GPTPartitionEntry {
     public String type;
     public long start;
     public long end;
-    public int sectors;
+    public long lbaSectors;
     public String size;
 
 
@@ -22,7 +22,42 @@ public class GPTPartitionEntry {
         buffer.position(buffer.position() + 32);
         start = buffer.getLong();
         end = buffer.getLong();
+        lbaSectors = (end - start) + 1;
+        size = getPartitionSize(lbaSectors);
 
-        System.out.println("Start: " + start + " | " + "End: " + end);
+
+        System.out.println("Start: " + start + " | " + "End: " + end + " | " + "Sectors: " + lbaSectors + " | " + "Size: " + size);
+    }
+
+    private String getPartitionSize(long sectors) {
+        return bytesIntoHumanReadable(sectors * 512);
+    }
+
+    private String bytesIntoHumanReadable(double bytes) {
+        double kilobyte = 1024;
+        double megabyte = kilobyte * 1024;
+        double gigabyte = megabyte * 1024;
+        double terabyte = gigabyte * 1024;
+
+        NumberFormat formatter = new DecimalFormat("#0.0");
+
+        if ((bytes >= 0) && (bytes < kilobyte)) {
+            return bytes + " B";
+
+        } else if ((bytes >= kilobyte) && (bytes < megabyte)) {
+            return formatter.format(bytes / kilobyte) + " KB";
+
+        } else if ((bytes >= megabyte) && (bytes < gigabyte)) {
+            return formatter.format(bytes / megabyte) + " MB";
+
+        } else if ((bytes >= gigabyte) && (bytes < terabyte)) {
+            return formatter.format(bytes / gigabyte) + " GB";
+
+        } else if (bytes >= terabyte) {
+            return formatter.format(bytes / terabyte) + " TB";
+
+        } else {
+            return bytes + " Bytes";
+        }
     }
 }
