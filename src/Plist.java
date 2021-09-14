181202194a2cb242c3aab2eb8097ab9707576daf
Mat Seto <seanseto@gmail.com>
2021-09-14 13:37:04 -0700

    feat (Plist.java): Parse data xml

--- src/Plist.java
+++ src/Plist.java
@@ -1,6 +1,12 @@
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Base64;
 
 public class Plist {
 
@@ -8,7 +14,30 @@ public class Plist {
 
     public Plist(byte[] pListBytes) {
         originalPlistBytes = pListBytes;
-        System.out.println(new String(pListBytes));
+        String plistXML = new String(pListBytes);
+//        System.out.println(plistXML);
+
+        DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
+
+        try {
+            DocumentBuilder builder = f.newDocumentBuilder();
+            InputStream is = new ByteArrayInputStream(pListBytes);
+            Document d = builder.parse(is);
+//            d.getDocumentElement().normalize();
+
+            NodeList dataDocs = d.getElementsByTagName("data");
+            int dataLen = dataDocs.getLength();
+//            String[] data = new String[dataLen];
+            for (int i = 0; i < dataLen; i++) {
+                // Todo: Parse data from each data entry's bytes
+
+//                data[i] = dataDocs.item(i).getTextContent();
+                String s = dataDocs.item(i).getTextContent().replaceAll("[\\n\\s]", "");
+                System.out.println(Arrays.toString(Base64.getDecoder().decode(s)));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     @Override
