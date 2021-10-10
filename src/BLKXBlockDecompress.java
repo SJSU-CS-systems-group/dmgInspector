832d8f0eae682e566c236080def0413f7acbfd45
Mat Seto <seanseto@gmail.com>
2021-10-10 15:21:18 -0700

    Finish byte buffer refactoring

--- src/BLKXBlockDecompress.java
+++ src/BLKXBlockDecompress.java
@@ -1,5 +1,4 @@
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 import java.util.zip.Inflater;
 
 
@@ -7,10 +6,14 @@ public class BLKXBlockDecompress {
     private static final int SECTOR_SIZE = 512; // not sure if this is the correct sector size.
 
     public static String decompressBLKXBlock(ByteBuffer dataForkBuffer, MishBlock.BLKXChunkEntry block) throws Exception {
-        byte[] compressedBytes = dataForkBuffer.slice((int) block.CompressedOffset, (int) block.CompressedLength).array();
         // Don't decompress if the bytes aren't zlib compressed.
         if (block.EntryType != 0x80000005) // hex code for zlib compression
-            throw new Exception("Non-zLib compressed bytes with type " + String.format("%08X", block.EntryType) +  ". Skipping decompression.");
+            throw new Exception("Non-zLib compressed bytes with type " + String.format("%08X", block.EntryType) + ". Skipping decompression.");
+
+        ByteBuffer compressedChunkBytesBuffer = dataForkBuffer.slice((int) block.CompressedOffset, (int) block.CompressedLength);
+        System.out.println(compressedChunkBytesBuffer.remaining());
+        byte[] compressedBytes = new byte[compressedChunkBytesBuffer.remaining()];
+        compressedChunkBytesBuffer.get(compressedBytes);
 
         Inflater decompresser = new Inflater();
         decompresser.setInput(compressedBytes, 0, compressedBytes.length);
--- src/Plist.java
+++ src/Plist.java
@@ -1,46 +1,45 @@
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.InputStream;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 public class Plist {
 
     public Plist(ByteBuffer pListBuffer, ByteBuffer dataForkBuffer) {
 //        String plistXML = new String(pListBuffer.array());
 //        System.out.println(plistXML);
-        System.out.println(pListBuffer.arrayOffset());
-        DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
 
         try {
-            DocumentBuilder builder = f.newDocumentBuilder();
-            InputStream is = new ByteArrayInputStream(pListBuffer.array());
-            Document d = builder.parse(is);
+            Document d = getPlistXMLDocument(pListBuffer);
 
-            NodeList dataDocs = d.getElementsByTagName("data");
-            int dataLen = dataDocs.getLength();
+            List<String> base64Data = getDataByKeyName(d, "Data");
+            List<String> cfNames = getDataByKeyName(d, "CFName");
 
-            File decompressedOutputFolder = new File("./output");
-            if (decompressedOutputFolder.exists()) {
-                decompressedOutputFolder.delete();
-            }
-            decompressedOutputFolder.mkdir();
+            int numMishBlocks = cfNames.size();
 
-            for (int mishBlockIndex = 0; mishBlockIndex < dataLen; mishBlockIndex++) {
-                String s = dataDocs.item(mishBlockIndex).getTextContent().replaceAll("[\\n\\s]", "");
+            File decompressedOutputFolder = createDecompressedOutputFolder();
+
+            for (int i = 0; i < numMishBlocks; i++) {
+                String s = base64Data.get(i).replaceAll("[\\n\\s]", "");
                 byte[] mishBytes = Base64.getDecoder().decode(s);
                 MishBlock block = new MishBlock(mishBytes);
+                System.out.println(block);
 
                 // DECOMPRESS
                 MishBlock.BLKXChunkEntry[] blkxChunks = block.getBlkxChunkEntries();
 
-                File decompressedMishFile = new File("./output/decompressed" + mishBlockIndex );
+                String fileName = i + "_"+cfNames.get(i).replaceAll("[\\(\\):\s]", "");
+                String pathName = decompressedOutputFolder.getCanonicalPath() + File.separator + fileName;
+                File decompressedMishFile = new File(pathName);
                 FileWriter decompressedChunkWriter = new FileWriter(decompressedMishFile.getAbsolutePath());
 
                 if (blkxChunks.length > 0) {
@@ -60,6 +59,66 @@ public class Plist {
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
+
+    /**
+     * @param pListBuffer ByteBuffer slice of plist bytes
+     * @return parsed XML Document
+     */
+    private Document getPlistXMLDocument(ByteBuffer pListBuffer) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        byte[] plistBytes = new byte[pListBuffer.remaining()];
+        pListBuffer.get(plistBytes);
+        InputStream is = new ByteArrayInputStream(plistBytes);
+        Document d = builder.parse(is);
+        return d;
+    }
+
+    /*
+    Get values based on their key
+
+    Examples:
+        ...
+        <key>CFName</key>
+        <string>Protective Master Boot Record (MBR : 0)</string>
+        ...
+
+
+                ...
+        <key>Data</key>
+        <string>   base64 Mish block data here   </string>
+        ...
+    */
+    private List<String> getDataByKeyName(Document d, String keyName) {
+        NodeList keys = d.getElementsByTagName("key");
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < keys.getLength(); i++) {
+
+            Node item = keys.item(i);
+            if (item.getTextContent().equals(keyName)) {
+                // For some reason, key data is two nodes after the key... we'd think it would be the next immediate sibling.
+                // This is likely a Java XML Document issue?
+                Node desiredNode = item.getNextSibling().getNextSibling();
+                String textContent = desiredNode.getTextContent();
+                data.add(textContent);
+            }
+        }
+
+        return data;
+    }
+
+    /**
+     * Creates a new decompressed output folder. The folder is deleted and re-created if it already exists.
+     *
+     * @return decompressed output folder reference
+     */
+    private File createDecompressedOutputFolder() {
+        File decompressedOutputFolder = new File("./output");
+        if (decompressedOutputFolder.exists()) {
+            decompressedOutputFolder.delete();
+        }
+        decompressedOutputFolder.mkdir();
+        return decompressedOutputFolder;
     }
 
     @Override
